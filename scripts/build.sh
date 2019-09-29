198aef7020e4932827fe29cb3c82355e55f88c74
waleCloud <walexrvp@gmail.com>
2019-09-29 11:52:49 +0100

    build and deploy scripts with travis config with env values

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,13 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 if [ "$TRAVIS_BRANCH" = "master" ]; then
   TAG="latest"
+  HEROKU_APP_NAME="$HEROKU_PROD_NAME"
 elif [ "$TRAVIS_BRANCH" = "staging" ]; then
   TAG="staging"
+  HEROKU_APP_NAME="$HEROKU_STAGING_NAME"
 else
   TAG="$TRAVIS_BRANCH"
+  HEROKU_APP_NAME="$TRAVIS_BRANCH"
 fi
-USER="walecloud/lepsta"
-docker build -f Dockerfile -t $USER:$TAG .
-docker tag $USER:$TAG registry.heroku.com/$HEROKU_APP_NAME/web
+docker build -f Dockerfile -t $DOCKER_REPO_NAME:$TAG .
+docker tag $DOCKER_REPO_NAME:$TAG registry.heroku.com/$HEROKU_APP_NAME/web
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,15 @@ heroku plugins:install @heroku-cli/plugin-container-registry
 # deploy to docker & heroku registy
 if [ "$TRAVIS_BRANCH" = "master" ]; then
   TAG="latest"
+  HEROKU_APP_NAME="$HEROKU_PROD_NAME"  
 elif [ "$TRAVIS_BRANCH" = "staging" ]; then
   TAG="staging"
+  HEROKU_APP_NAME="$HEROKU_STAGING_NAME"
 else
   TAG="$TRAVIS_BRANCH"
+  HEROKU_APP_NAME="$TRAVIS_BRANCH"
 fi
-USER="walecloud/lepsta"
-docker push $USER:$TAG
+docker push $DOCKER_REPO_NAME:$TAG
 docker push registry.heroku.com/$HEROKU_APP_NAME/web;
 # Deploy docker container as a web app and release it
 heroku container:push web --app $HEROKU_APP_NAME
